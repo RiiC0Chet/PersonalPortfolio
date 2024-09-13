fc65fa2048f1f02f42333ab011224eedf2138b7f
RiiC0Chet <jlricoramos@correo.ugr.es>
2024-09-13 12:16:22 +0100

    Bug fixes

--- railsApp/app/javascript/contact_form.js
+++ railsApp/app/javascript/contact_form.js
@@ -18,19 +18,38 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   document.addEventListener("DOMContentLoaded", function() {
-
+    // Selecciona el enlace del botón "Contacto"
     const contactButton = document.querySelector(".nav-button[href*='#contact-form']");
+  
+    
+  
+    // Función para manejar el desplazamiento y mostrar el formulario
+    function handleScrollToContactForm() {
 
-      contactButton.addEventListener("click", function(event) {
-        const contactForm = document.getElementById("contact-form-container");
-        const footer = document.querySelector(".site-footer");
-        event.preventDefault(); 
-        console.log(contactForm);
-        contactForm.style.display = "block";
+      const contactForm = document.getElementById("contact-form-container");
+      contactForm.style.display = "block";
+      // Muestra el formulario de contacto después de un pequeño retraso
+      setTimeout(function() {
         
-        setTimeout(function() {
-          footer.scrollIntoView({ behavior: "smooth" });
-        }, 500);
-
-      });
+        // Desplaza la página hasta el formulario de contacto
+        contactForm.scrollIntoView({ behavior: "smooth" });
+      }, 500); // Espera 500ms para asegurarte de que el desplazamiento haya terminado
+    }
+  
+    // Añade un evento de clic al botón "Contacto"
+    contactButton.addEventListener("click", function(event) {
+      event.preventDefault(); // Previene la acción predeterminada del enlace
+  
+      // Redirige a la página root con el ancla del formulario de contacto
+      window.location.href = root_path + "#contact-form";
+  
+      // Maneja el desplazamiento y muestra el formulario
+      handleScrollToContactForm();
+    });
+  
+    // Verifica si la página se ha cargado con un ancla específica
+    if (window.location.hash === "#contact-form") {
+      // Maneja el desplazamiento y muestra el formulario
+      handleScrollToContactForm();
+    }
   });
